b97cc6e36a6cf208e7c696195bd9f8293ce03327
Piotr Mankowski <pmanko@uw.edu>
2021-08-08 04:14:36 -0700

    Test fix

--- src/__tests__/lab.test.ts
+++ src/__tests__/lab.test.ts
@@ -10,8 +10,12 @@ describe('translateLabOrderBundle',
       let testBundle: R4.IBundle = await got.get("https://i-tech-uw.github.io/laboratory-workflows-ig/Bundle-example-laboratory-simple-bundle.json").json()
 
       const result = translateLabOrderBundle(testBundle);
+      
+      let p: R4.IPatient = <R4.IPatient>testBundle.entry![1].resource!
 
       expect(result).toContain('|');
+      expect(result).toContain(p.name![0].family!)
       expect(result).toContain(testBundle.entry![1].resource!.id!) 
   }); 
 });
+
